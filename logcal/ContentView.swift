2b0f4440ea1bf88f3a8d6541959a280651ad838f
ajohri <johriantriksh24@gmail.com>
2025-12-15 03:51:33 +0530

    Initial Commit

---
+++ logcal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  logcal
+//
+//  Created by Antriksh Johri on 15/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ logcal/logcalApp.swift
@@ -0,0 +1,17 @@
+//
+//  logcalApp.swift
+//  logcal
+//
+//  Created by Antriksh Johri on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct logcalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
