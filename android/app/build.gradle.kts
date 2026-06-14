bba785db0ec61802341a2919f4a52a01f6989eee
Antriksh Johri <johriantriksh24@gmail.com>
2026-06-14 05:57:00 +0530

    Removed fonts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.ui:ui-text-google-fonts")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("io.coil-kt:coil-compose:2.7.0")
